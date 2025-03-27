47395487d78dfdd84f8f45e6fd45906e8dce6e5a
kunalGonge <kunalgonge349@gmail.com>
2025-03-27 11:26:06 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -4,15 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://w
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 // Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBMdKmv7zXaM28Qg6oTGJmjIFDsSxs8DEQ",
-  authDomain: "task-manager-c98cd.firebaseapp.com",
-  projectId: "task-manager-c98cd",
-  storageBucket: "task-manager-c98cd.firebasestorage.app",
-  messagingSenderId: "846818943768",
-  appId: "1:846818943768:web:32bedbea79b71e86e6346d",
-  measurementId: "G-99CZTWYJ7Q"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
